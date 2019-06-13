193f838f6118cce0b9422644e74892de67df003f
rupuzhao <51755308+rupuzhao@users.noreply.github.com>
2019-06-12 21:50:04 -0400

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_9_BusDriverDB
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void busBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.busBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.busDriverDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'busDriverDataSet.BusModel' table. You can move, or remove it, as needed.
+            this.busModelTableAdapter.Fill(this.busDriverDataSet.BusModel);
+            // TODO: This line of code loads data into the 'busDriverDataSet.Bus' table. You can move, or remove it, as needed.
+            this.busTableAdapter.Fill(this.busDriverDataSet.Bus);
+
+        }
+    }
+}
