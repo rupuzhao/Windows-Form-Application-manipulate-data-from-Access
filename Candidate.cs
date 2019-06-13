ae51c9c1efb6490df97a116d9822ebffd139b6f1
rupuzhao <51755308+rupuzhao@users.noreply.github.com>
2019-06-12 21:51:51 -0400

    Add files via upload

---
+++ Candidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_9_BusDriverDB
+{
+    public class Candidate
+    {
+        public Candidate(string driverId, string name, string age)
+        {
+            this.DriverID = driverId;
+            this.Name = name;
+            this.Age = age;
+        }
+        public string DriverID { set; get; }
+        public string Name { set; get; }
+        public string Age { set; get; }
+
+        public string Details()
+        {
+            return DriverID + Name + Age;
+        }
+    }
+}
